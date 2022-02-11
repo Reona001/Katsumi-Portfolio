fdb979cb4d0482c68a8228765939f4a6aa2922b8
reona kasuya <reon.kasuya@gmail.com>
2022-02-11 19:43:06 +0900

    project and user created

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,10 @@
 class Project < ApplicationRecord
+  validates :title, presence: true
+  validates :created, presence: true
+  validates :created_by, presence: true
+  validates :stack_used, presence: true
+  validates :url, presence: true
+  validates :url, presence: true
+  validates :git_repo_url, presence: true
   belongs_to :user
 end
