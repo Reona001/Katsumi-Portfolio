7eb9568f635485d529490ad63a70b97aeecba2cd
reona kasuya <reon.kasuya@gmail.com>
2022-02-13 23:08:41 +0900

    basic user profile created

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home' # this sets localhost:3000/pages/home as root path
   resources :projects, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-
+  resources :users, only: [:show, :edit, :update]
   # if something has sub-elements nest it using do
 end
