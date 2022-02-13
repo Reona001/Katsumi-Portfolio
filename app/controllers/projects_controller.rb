dbc09ab061aaf775ba39fd21c0c765ce67ab0906
reona kasuya <reon.kasuya@gmail.com>
2022-02-13 21:04:39 +0900

    Destroy feature completed

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,49 @@
 class ProjectsController < ApplicationController
+  before_action :find_project, only: [:show, :edit, :update, :destroy]
+
   def index
     @projects = Project.all
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def show
+  end
+
+  def create
+    @project = Project.new(project_params)
+    @project.user = current_user
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @project.update(project_params)
+      redirect_to project_path(@project), notice: "Project Created!"
+    else
+      render :new
+    end
+  end
+
+    def destroy
+      @project.destroy
+      redirect_to projects_path, notice: "Project Removed!"
+    end
+
+    private
+
+    def find_project
+      @project = Project.find(params[:id])
+    end
+
+    def project_params
+      params.require(:project).permit(:title, :created, :created_by, :stack_used, :url, :git_repo_url)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'pages#home' # this sets localhost:3000/pages/home as root path
-
-  resources :projects
+  resources :projects, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 
   # if something has sub-elements nest it using do
 end
