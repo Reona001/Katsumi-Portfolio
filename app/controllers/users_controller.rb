ef5cf7a36a9b6e953052de41e56ede8ef228a53c
reona kasuya <reon.kasuya@gmail.com>
2022-02-14 23:57:36 +0900

    user profile photo upload feature created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:username, :image)
+    params.require(:user).permit(:username, :image, :photo)
   end
 
 end
