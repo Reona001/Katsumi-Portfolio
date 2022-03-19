f4621e20eddb4435d464404d7445ed335c37bda1
reona kasuya <reon.kasuya@gmail.com>
2022-03-19 17:23:54 +0900

    added index template for blogposts

--- config/routes.rb
+++ config/routes.rb
@@ -14,14 +14,13 @@ Rails.application.routes.draw do
   # end
 
   # devise_for :users
-
   get '/profile', to: "users#profile"
-  get '/blog', to: "users#blog"
 
   root to: 'pages#home' # this sets localhost:3000/pages/home as root path
   resources :projects, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   resources :users, only: [:show, :edit, :update]
   resources :experiences, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :blogposts
 
   resources :chatrooms, only: [:show, :new, :create, :index] do
     resources :messages, only: [:create, :destroy]
