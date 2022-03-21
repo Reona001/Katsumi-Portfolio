525556896b22535437c6df224cd0f4d5a55707fe
reona kasuya <reon.kasuya@gmail.com>
2022-03-21 16:21:37 +0900

    user profile of myself made visible

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
+
   def home
-    @users = User.all
+    @user = User.find_by(username: 'Reona')
+    # Using id: 3 could cause errors when db:drops
   end
+
+  # Documentation on find_by and where
+  # https://qiita.com/nakayuu07/items/3d5e2f8784b6f18186f2
 end
