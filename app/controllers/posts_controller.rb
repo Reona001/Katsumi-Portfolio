b5f1112905112e587e7d1cbe8a2f1725e28c7c9b
reona kasuya <reon.kasuya@gmail.com>
2022-03-26 23:48:55 +0900

    Blog post pages code cleaned and functional#

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,21 +3,20 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
     @posts = Post.all
-    # @blogpost = Blogpost.find(params[:format])
+    @blogpost = Blogpost.find(params[:format])
 
   end
 
   def create
-    # @blogpost = Blogpost.find(params[:format])
     @post = Post.new(post_params)
-    # session[:blogpost_id] = @post.blogpost_id
     @blogpost = @post.blogpost_id
     if @post.save
-      # @post.user = current_user
       redirect_to new_blogpost_path
     else
       render :new
     end
+
+
   end
 
   private
