6f3b9157d0ff2a2365c4f78da7c8682c4547d714
reona kasuya <reon.kasuya@gmail.com>
2022-04-10 23:42:59 +0900

    cloudinary config in cloudinary.rb

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dvdm3zjhh'
+  config.api_key = '119232245488563'
+  config.api_secret = 'a8VxDywn8j6DueoARX-mRX_myUA'
+  config.secure = true
+  config.cdn_subdomain = true
+end
